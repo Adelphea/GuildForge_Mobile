b1460667c38ecef97efa12f1ecf2f7d753e0b292
Adelphea <207628645+Adelphea@users.noreply.github.com>
2025-11-29 00:03:20 -0500

    fix: Final correction to Hybrid Strategy - All complex types are now empty shells (IntRange, Color, etc.) for maximum fidelity.

--- GuildForge/scripts/resources/Color.gd
+++ GuildForge/scripts/resources/Color.gd
@@ -2,10 +2,6 @@
 class_name Color
 extends Resource
 
-@export var r: float = 0.0
-@export var g: float = 0.0
-@export var b: float = 0.0
-@export var a: float = 0.0
 
 func validate() -> void:
 	pass
--- GuildForge/scripts/resources/IntVec2.gd
+++ GuildForge/scripts/resources/IntVec2.gd
@@ -2,8 +2,6 @@
 class_name IntVec2
 extends Resource
 
-@export var x: int = 0
-@export var z: int = 0
 
 func validate() -> void:
 	pass
